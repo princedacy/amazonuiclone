fbf84a8a4af467be8851a90d7da6adedbd11fa67
Dacy <princedacy@gmail.com>
2023-03-29 09:19:06 +0200

    feat: header layout and start product feed

---
+++ src/components/Product.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import { StarIcon } from "@heroicons/react/outline";
+import Currency from "react-currency-formatter"
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+function Product({ id, title, price, description, category, image }) {
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+  const [hasPrime] = useState(Math.random() < 0.5);
+  return (
+    <div>
+      <p>{category}</p>
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      <h4>{title}</h4>
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => {
+            return (<StarIcon className="h-5" />);
+          })}
+      </div>
+      <p>{description}</p>
+      <div>
+        <Currency quantity={price} currency="USD" />
+      </div>
+      {hasPrime && (
+        <div>
+            <img src="https://links.papareact.com/fdw" alt="" />
+            <p>FREE Next-day Delivery</p>
+        </div>
+      )}
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
